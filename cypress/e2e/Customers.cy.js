24cd096d667a26b7610de97e49cd4d10045954ce
unknown <doukaliomarpro@gmail.com>
2024-04-04 02:53:13 +0000

    dd news

--- cypress/e2e/Customers.cy.js
+++ cypress/e2e/Customers.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/simple-reactjs-app/customerlist')
+    cy.get('.App-title').should('have.text' , 'Simple React App');
+    cy.get(':nth-child(1) > .panel-body > .btn');
+    cy.get('.customerdetails > .centeralign > .panel-heading > .panel-title').should('have.text' , 'Doukali Omar');
+    cy.get(':nth-child(2) > .panel-body > .btn').click();
+    cy.get('.customerdetails > .centeralign > .panel-heading > .panel-title').should('have.text','ABCD');
   })
 })
